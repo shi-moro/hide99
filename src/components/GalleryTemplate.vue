f7a27f2fef34e7f06358a197f50cfa76c9dccd1b
Shimoro-Rune <DinaraWolfQuest2013@yandex.ru>
2023-06-16 17:11:04 +0300

    test

---
+++ src/components/GalleryTemplate.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-row>
+    <v-col
+      v-for="n in 9"
+      :key="n"
+      class="d-flex child-flex"
+      cols="4"
+    >
+      <v-img
+        :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
+        :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
+        aspect-ratio="1"
+        cover
+        class="bg-grey-lighten-2"
+      >
+        <template v-slot:placeholder>
+          <v-row
+            class="fill-height ma-0"
+            align="center"
+            justify="center"
+          >
+            <v-progress-circular
+              indeterminate
+              color="grey-lighten-5"
+            ></v-progress-circular>
+          </v-row>
+        </template>
+      </v-img>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import { sql } from "@vercel/postgres";
+
+export default {
+  name: "GalleryTemplate",
+
+  async mounted() {
+    const {rows} = await sql`SELECT *
+                             from users`;
+
+    console.log(rows);
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <HelloWorld />
+  <GalleryTemplate />
 </template>
 
 <script setup>
   import HelloWorld from '@/components/HelloWorld.vue'
+  import GalleryTemplate from "@/components/GalleryTemplate.vue";
 </script>
